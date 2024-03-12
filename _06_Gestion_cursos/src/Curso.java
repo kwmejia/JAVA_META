5e6c8f5e0ded331f9ea07293631678ddb8638a42
Kevin Mejia <kwmejia9@gmail.com>
2024-03-12 08:31:23 -0500

    Agregando 06

---
+++ _06_Gestion_cursos/src/Curso.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Curso {
+    private  String codigo;
+    private  String nombre;
+    private ArrayList<Estudiante> listaEstudiantes;
+    private static int idEstudiante = 1;
+
+    public Curso(String codigo, String nombre) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.listaEstudiantes = new ArrayList<>();
+    }
+
+    public void guardarEstudiante(Scanner objScan){
+        System.out.println("Agregar nuevo estudiante\n");
+        System.out.println("Ingresa el nombre del estudiante: ");
+        String nombre = objScan.nextLine();
+
+        System.out.println("Ingresa el email del estudiante: ");
+        String email = objScan.next();
+
+        Estudiante objEstudiante = new Estudiante(idEstudiante,nombre,email);
+        idEstudiante++;
+
+        if (this.listaEstudiantes.add(objEstudiante)){
+            System.out.println("Estudiante agregado correctamente!");
+        }else{
+            System.out.println("No se pudo agregar al estudiante 😒");
+        }
+    }
+
+    public void listarEstudiantes(){
+        //Validar si la lista está vacía
+        if (this.listaEstudiantes.isEmpty()){
+            System.out.println("El curso ".concat(this.nombre).concat(" No tiene estudiantes"));
+        }else {
+
+            System.out.println("\nLista de estudiantes en el curso - "+ this.nombre);
+            for (Estudiante estudianteTemp : this.listaEstudiantes){
+                System.out.println(estudianteTemp.toString());
+            }
+        }
+    }
+
+    public  void eliminarEstudiante(Scanner objScan){
+        //1. Listar
+        this.listarEstudiantes();
+        //2. Pedir El identificador
+        System.out.println("Ingresa el identificador del usuario a eliminar: ");
+        int idEliminar = objScan.nextInt();
+
+        //3. Eliminar
+        if ( this.listaEstudiantes.removeIf(estudiante -> estudiante.getId() == idEliminar)){
+            System.out.println("Estudiante eliminado correctamente");
+        }else{
+            System.out.println("No se pudo eliminar el estudiante");
+        }
+
+    }
+
+
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public ArrayList<Estudiante> getListaEstudiantes() {
+        return listaEstudiantes;
+    }
+
+    public void setListaEstudiantes(ArrayList<Estudiante> listaEstudiantes) {
+        this.listaEstudiantes = listaEstudiantes;
+    }
+}
---
+++ _06_Gestion_cursos/src/GestionCurso.java
@@ -0,0 +1,46 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestionCurso {
+    private ArrayList<Curso> listaCursos;
+
+    public GestionCurso(){
+        this.listaCursos = new ArrayList<>();
+    }
+
+    public void guardarCurso(Scanner objScan){
+        System.out.println("Agregar Curso");
+
+        System.out.println("Ingresa el nombre del curso: ");
+        String nombre = objScan.nextLine();
+
+        System.out.println("Ingresa el código del curso");
+        String codigo = objScan.next();
+        //Si el método buscar no devuelve null quiere decir que ese código ya existe
+        if (this.buscarPorCodigo(codigo) != null){
+            System.out.println("El código ya existe ");
+        }else {
+            //Creamos el curso
+            Curso objCurso = new Curso(codigo,nombre);
+            this.listaCursos.add(objCurso);
+        }
+    }
+
+
+    public Curso buscarPorCodigo(String codigoBuscar){
+        for (Curso cursoTemp: this.listaCursos){
+            if (cursoTemp.getCodigo().equalsIgnoreCase(codigoBuscar)){
+                return cursoTemp;
+            }
+        }
+        return null;
+    }
+    public ArrayList<Curso> getListaCursos() {
+        return listaCursos;
+    }
+
+    public void setListaCursos(ArrayList<Curso> listaCursos) {
+        this.listaCursos = listaCursos;
+    }
+}
